14a22db19f39f4ed9228fcf3aa0b3c3eff39ffeb
chopi <chopinguy@cox.net>
2021-07-28 15:56:16 -0700

    7/28/2021 3:56PM

---
+++ Driver.py
@@ -0,0 +1,3 @@
+import mainGame
+g = mainGame.Game()
+g.begin()
--- mainGame.py
+++ mainGame.py
@@ -1,61 +1,102 @@
 from france import france
 from player import player
+import threading
+from time import *
 from room import room
+import sys
 
+class Game:
 
-France = france()
-France.setupLocation()
-Player = player(France.initRoom, France)
+    def __init__(self):
+        self.France = france()
+        self.France.setupLocation()
+        self.Player = player(self.France.initRoom, self.France)
+        self.my_timer = 5
+        self.countdown_thread = threading.Thread(target=self.timer)
+        self.quit = False
 
-def test():
-    test
+    def begin(self):
+        self.countdown_thread.start()
+        self.command()
 
-def command():
-    quit = True
-    cmd = ""
-    cmd = input("<TimeLoop>")
-    cmd = cmd.split(" ")
-    # print(cmd)
-    if cmd[0] == "pickup" and len(cmd) == 2:
-        print(Player.pickup(cmd[1]))
-    elif cmd == ["status"]:
-        print(Player.status())
-    elif (cmd[0] == "view" or cmd[0] == "v") and len(cmd) == 2:
-        print(Player.viewItem(cmd[1]))
-    elif cmd == ["view"] or cmd == ["v"]:
-        Player.viewRoom()
-    elif cmd[0] == "use" and len(cmd) == 3:
-        print(Player.use(cmd[1], cmd[2]))
-    elif cmd[0] == "combine" and len(cmd) == 3:
-        print(Player.combine())
-    elif (cmd[0] == "move" or cmd[0] == "m") and len(cmd) == 2:
-        Player.move(cmd[1])
-    elif cmd[0] == "eat" and len(cmd) == 2:
-        print(Player.eat(cmd[1]))
-    elif cmd[0] == "drink" and len(cmd) == 2:
-        print(Player.drink(cmd[1]))
-    elif cmd[0] == "discard" and len(cmd) == 2:
-        print(Player.discard())
-    elif cmd[0] == "backpack" or cmd[0] == "bp" and len(cmd) == 1:
-        Player.viewBackpack()
-    elif cmd == ["room"]:
-        print(Player.currentRoom.description)
-    elif cmd == ["rooms"]:
-        Player.printAdjacentRoomList()
-    elif (cmd[0] == "talkto" or cmd[0] == "t") and len(cmd) == 2:
-        Player.talk(cmd[1])
-    elif cmd[0] == "quit":
-        quit = False
-    elif cmd[0] == "help" and len(cmd) == 2:
-        Player.help(cmd[1])
-    elif cmd == ["?"]:
-        Player.commands()
-    else:
-        print("unknown command")
-    
-    if quit:
-        command()
+    def timer(self):
+        for x in range(5):
+            self.my_timer -= 1
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+            sleep(.1)
+            if self.quit:
+                break
+        if not self.quit:
+            print("\n24 hours have passed. You wake up back in your bed in France.\nPress Enter to continue")
+            self.my_timer = 5
+            self.timer()
 
-# while(quit):
-#     command()
-command()
+    def command(self):
+        while not self.quit:
+            cmd = ""
+            cmd = input("<TimeLoop>")
+            cmd = cmd.split(" ")
+            if cmd[0] == "pickup" and len(cmd) == 2:
+                print(self.Player.pickup(cmd[1]))
+            elif cmd == ["status"]:
+                print(self.Player.status())
+            elif (cmd[0] == "view" or cmd[0] == "v") and len(cmd) == 2:
+                print(self.Player.viewItem(cmd[1]))
+            elif cmd == ["view"] or cmd == ["v"]:
+                self.Player.viewRoom()
+            elif cmd[0] == "use" and len(cmd) == 3:
+                print(self.Player.use(cmd[1], cmd[2]))
+            elif cmd[0] == "combine" and len(cmd) == 3:
+                print(self.Player.combine())
+            elif (cmd[0] == "move" or cmd[0] == "m") and len(cmd) == 2:
+                self.Player.move(cmd[1])
+            elif cmd[0] == "eat" and len(cmd) == 2:
+                print(self.Player.eat(cmd[1]))
+            elif cmd[0] == "drink" and len(cmd) == 2:
+                print(self.Player.drink(cmd[1]))
+            elif cmd[0] == "discard" and len(cmd) == 2:
+                print(self.Player.discard())
+            elif cmd[0] == "backpack" or cmd[0] == "bp" and len(cmd) == 1:
+                self.Player.viewBackpack()
+            elif cmd == ["room"]:
+                print(self.Player.currentRoom.description)
+            elif cmd == ["rooms"]:
+                self.Player.printAdjacentRoomList()
+            elif (cmd[0] == "talkto" or cmd[0] == "t") and len(cmd) == 2:
+                self.Player.talk(cmd[1])
+            elif cmd[0] == "quit":
+                self.quit = True
+            elif cmd[0] == "help" and len(cmd) == 2:
+                self.Player.help(cmd[1])
+            elif cmd == ["?"]:
+                self.Player.commands()
+            else:
+                print("unknown command")
+        self.countdown_thread.join()
+
